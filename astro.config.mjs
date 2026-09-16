49add114983958ec7155cec0b9a49098cb54fb92
karonte1 <duquejuanjose19@gmail.com>
2026-09-16 22:21:46 +0200

    añadir workflo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://karonte1.github.io',
-  base: '/porfolio_juan', // Ejemplo: '/portafolio' (si tu repo se llama así)
+  site: 'https://Karonte1.github.io',
+  base: '/porfolio_juan',
 });
