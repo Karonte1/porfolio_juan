27415a5a26c73339ac24ed0a29ee1b8dfeb118bd
karonte1 <duquejuanjose19@gmail.com>
2026-09-16 22:10:52 +0200

    github pages cambios

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://karonte1.github.io',
+  base: '/porfolio_juan', // Ejemplo: '/portafolio' (si tu repo se llama así)
+});
